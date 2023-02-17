f748fedeaeea2dbbd417eedc03354cf25de16399
Ehab98 <hoba.reda98@gmail.com>
2023-02-17 17:49:50 +0200

    Customize backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -51,7 +51,7 @@ function ProductScreen({match}) {
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className='btn-block' type='button'> Add To cart
+                <Button className='btn-block' type='button' disabled={product.countInStock === 0}> Add To cart
 
                 </Button>
               </ListGroup.Item>
