d1badba0aa4e4e9e5e5fb081ab8b5b990db5a06e
Ehab98 <hoba.reda98@gmail.com>
2023-02-17 20:38:32 +0200

    Data from Django

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('_id','user','name')
+
+admin.site.register(Product, ProductAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('_id','user','totalPrice')
+
+admin.site.register(Order, OrderAdmin)
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('_id','product','name')
+
+admin.site.register(OrderItem, OrderItemAdmin)
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    list_display = ('_id','address','ShippingPrice')
+
+admin.site.register(ShippingAddress, ShippingAddressAdmin)
